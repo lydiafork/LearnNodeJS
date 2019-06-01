09a610fb35918a9db9d9fb27b97d60a35311b9a8
zml <meiling.zheng@beibei.com>
2019-06-01 16:56:12 +0800

    nodejs 学习demo2

--- demo02/ecosystem.config.js
+++ demo02/ecosystem.config.js
@@ -7,16 +7,22 @@ module.exports = {
         args: 'one two',
         instances: 1,
         autorestart: true,
-        watch: false,
+        watch: true, // 启用或禁用观察模式	
+	    output: './out.log', // studout的文件路径（每行都附加到该文件中) ~/.pm2/logs/-out.log
+        error: './error.log', // stderr的文件路径（每行都附加到此文件中） ~/.pm2/logs/-error.err
+        log: './combined.outerr.log',
         max_memory_restart: '1G',
         env: {
             NODE_ENV: 'development'
         },
         env_production: {
             NODE_ENV: 'production'
-        }
+        },
     }],
     deploy : {
+        development:{
+
+        },
         production : {
             user : 'node',
             host : '212.83.163.1',
@@ -24,6 +30,6 @@ module.exports = {
             repo : 'git@github.com:repo.git',
             path : '/var/www/production',
             'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-        }
+        },
     }
 };
